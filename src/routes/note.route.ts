595e61643c685dc20b05f470ab3f385f38de26b6
Ayush Yadav <yadavayush94322@gmail.com>
2026-08-05 05:08:16 +0530

    feat: add get /notes route

--- src/routes/note.route.ts
+++ src/routes/note.route.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 import { NoteController } from "../controllers/note.controller.js";
 import { authenticate } from "../middlewares/auth.middleware.js";
 import { validate } from "../middlewares/validate.js";
-import { createNoteSchema } from "../validators/note.validator.js";
+import { createNoteSchema, getNotesSchema } from "../validators/note.validator.js";
 
 const router = Router();
 
 const controller = new NoteController();
 
 router.post("/", authenticate, validate(createNoteSchema), controller.create.bind(controller));
+router.get("/", authenticate, validate(getNotesSchema, "query"), controller.findAll.bind(controller));
 
 export default router;
