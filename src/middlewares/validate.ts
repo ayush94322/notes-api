123b7536c764959c57490aa208c98336aa4658e7
Ayush Yadav <yadavayush94322@gmail.com>
2026-08-05 05:06:55 +0530

    refactor: generalise source for validate

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -1,10 +1,12 @@
 import {ZodType} from "zod";
 import type { RequestHandler } from "express";
 
-export function validate(schema: ZodType): RequestHandler {
+type RequestPart = "body" | "query" | "params";
+
+export function validate(schema: ZodType, source: RequestPart = "body"): RequestHandler {
     return (req, res, next)=>{
         try {
-            const result = schema.safeParse(req.body);
+            const result = schema.safeParse(req[source]);
             req.validatedData = result.data;
             next();
         } catch (error) {
