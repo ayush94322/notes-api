b871341c5c40d917ac3d891ba242d4112659c796
Ayush Yadav <yadavayush94322@gmail.com>
2026-08-08 23:14:00 +0530

    feat: add getById route

--- src/routes/note.route.ts
+++ src/routes/note.route.ts
@@ -2,13 +2,14 @@ import { Router } from "express";
 import { NoteController } from "../controllers/note.controller.js";
 import { authenticate } from "../middlewares/auth.middleware.js";
 import { validate } from "../middlewares/validate.js";
-import { createNoteSchema, getNotesSchema } from "../validators/note.validator.js";
+import { createNoteSchema, getNotesSchema, noteIdSchema } from "../validators/note.validator.js";
 
 const router = Router();
 
 const controller = new NoteController();
 
-router.post("/", authenticate, validate(createNoteSchema), controller.create.bind(controller));
+router.get("/:id", authenticate, validate(noteIdSchema, "params"), controller.findById.bind(controller));
 router.get("/", authenticate, validate(getNotesSchema, "query"), controller.findAll.bind(controller));
+router.post("/", authenticate, validate(createNoteSchema), controller.create.bind(controller));
 
 export default router;
