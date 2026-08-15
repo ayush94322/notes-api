e9c6532c93a069937d29c14ff83f956998549edc
Ayush Yadav <yadavayush94322@gmail.com>
2026-08-15 23:32:09 +0530

    refactor: add test scripts

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import helmet from "helmet";
+
 import noteRoutes from "./routes/note.route.js";
 import authRoutes from "./routes/auth.route.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
@@ -7,12 +9,13 @@ import { NotFoundError } from "./errors/NotFoundError.js";
 const app = express();
 
 //Middleware
+app.use(helmet());
 app.use(express.json());
 
 //Routing
 app.use("/notes", noteRoutes);
 app.use("/auth", authRoutes);
-app.get("/health", (req, res)=>{
+app.get("/health", (_req, res)=>{
     res.status(200).json({
         success: true,
         message: "Notes API is running"
